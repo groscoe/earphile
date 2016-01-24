8ea98eca6006c1205fd400ec8a1a5cc02d52426a
groscoe <olwenntaron@gmail.com>
2016-01-24 18:55:18 -0200

    Added simple switching animations

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 // Loading animation
+document.getElementById('dashboard').style.display = 'block'; // Show the dashboard section
 document.getElementsByTagName('html')[0].classList.remove('loading');
 
 // Sidebar navigation
 var switch_section = function (clicked_link, section_id) {
   if (!clicked_link.classList.contains('active-link')) { // if the section selected isn't active already
-    document.getElementsByClassName('active-link')[0].classList.toggle('active-link');
-    clicked_link.classList.toggle('active-link');
-    document.getElementsByClassName('active-section')[0].classList.toggle('active-section');
-    document.getElementById(section_id).classList.toggle('active-section');
+    document.getElementsByClassName('active-link')[0].classList.remove('active-link');
+    clicked_link.classList.add('active-link');
+
+    Velocity(document.getElementsByClassName('active-section')[0], "fadeOut", {
+      duration: 200
+    });
+    Velocity(document.getElementById(section_id), "fadeIn", {
+      delay: 200,
+      duration: 200
+    });
+
+    document.getElementsByClassName('active-section')[0].classList.remove('active-section');
+    document.getElementById(section_id).classList.add('active-section');
   }
 }
