0ae53d02e6bb5286266e4509f566ecce4e039f36
groscoe <olwenntaron@gmail.com>
2016-01-24 18:08:21 -0200

    Updated basic layout with some really basic 'single-page' style navigation. The '<h1>' headers are just placeholders.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+// Loading animation
+document.getElementsByTagName('html')[0].classList.remove('loading');
 
+// Sidebar navigation
+var switch_section = function (clicked_link, section_id) {
+  if (!clicked_link.classList.contains('active-link')) { // if the section selected isn't active already
+    document.getElementsByClassName('active-link')[0].classList.toggle('active-link');
+    clicked_link.classList.toggle('active-link');
+    document.getElementsByClassName('active-section')[0].classList.toggle('active-section');
+    document.getElementById(section_id).classList.toggle('active-section');
+  }
+}
